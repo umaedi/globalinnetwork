3c306396e1ec241a3dde825ffb0b51c3a619ab56
umaedi <umaedi.kh.99@gmail.com>
2025-05-06 21:43:38 +0700

    fix: format post date to localized string in news section

--- resources/views/web/home/_berita_terbaru.blade.php
+++ resources/views/web/home/_berita_terbaru.blade.php
@@ -52,7 +52,7 @@
                         <span class="badge bg-primary">{{ $category->nama_kategori }}</span>
                         <span class="text-muted text-sm">
                             <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" style="fill: rgba(0, 0, 0, 1);transform: ;msFilter:;"><path d="M12 2C6.486 2 2 6.486 2 12s4.486 10 10 10 10-4.486 10-10S17.514 2 12 2zm0 18c-4.411 0-8-3.589-8-8s3.589-8 8-8 8 3.589 8 8-3.589 8-8 8z"></path><path d="M13 7h-2v5.414l3.293 3.293 1.414-1.414L13 11.586z"></path></svg>
-                            {{ $bigPost->created_at }}</span>
+                            {{ {{ \Carbon\Carbon::parse($bigPost->created_at)->locale('id_ID')->isoFormat('dddd, D MMMM YYYY') }} }}</span>
                     </div>
                     <h5 class="card-title judu-berita"><a href="{{ route('berita.show', ['slug' => $bigPost->slug]) }}">{{ $bigPost->judul }}</a></h5>
                     <p class="excrept">{{ $bigPost->excrept }}</p>
