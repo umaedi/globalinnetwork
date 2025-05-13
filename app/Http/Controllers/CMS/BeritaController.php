1bf3fcc83e6ac7b055e6447a42ddb0adfea428b0
umaedi <umaedi.kh.99@gmail.com>
2025-05-13 16:37:31 +0700

    fix: update thumbnail upload handling in berita store method

--- app/Http/Controllers/CMS/BeritaController.php
+++ app/Http/Controllers/CMS/BeritaController.php
@@ -197,7 +197,7 @@ class BeritaController extends Controller
 
         $data['excrept'] = Str::limit(strip_tags($request->body), 200, '...');
         $data['user_id']    = $berita->user_id == Auth::id() ? Auth::id() : $berita->user_id;
-        $data['subdomain']  = getHost() ?? $berita->subdomain;
+        $data['subdomain']  = $berita->subdomain;
         $data['slug']       = Str::slug($request->judul);
         $data['caption_thumbnail']  = $request->caption_thumbnail ?? $request->input('judul');
         $data['tanggal_publish']    = $request->tanggal_publish ?? Carbon::now();
