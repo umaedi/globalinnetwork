814915ee4153a28e58ac3c5f08c9ed0e35aaf16f
umaedi <umaedi.kh.99@gmail.com>
2025-05-17 18:45:41 +0700

    fix: wrap category items in anchor tags for improved navigation

--- resources/views/web/home/_kategori.blade.php
+++ resources/views/web/home/_kategori.blade.php
@@ -1,7 +1,9 @@
 @forelse ($kategori as $item)
 <div class="d-flex align-items-center justify-content-between gap-3">
-    <p>{{ $item->nama_kategori }}</p>
-    <span class="badge bg-primary">{{ $item->posts_count }}</span>
+    <a href="/kategori/{{ $item->slug }}">
+        <p>{{ $item->nama_kategori }}</p>
+        <span class="badge bg-primary">{{ $item->posts_count }}</span>
+    </a>
 </div>
 @empty
     
