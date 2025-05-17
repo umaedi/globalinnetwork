11d4a17c1ea30602b2e8cf5d82a6a06162ca974c
umaedi <umaedi.kh.99@gmail.com>
2025-05-17 18:47:02 +0700

    fix: ensure category items are wrapped in anchor tags for improved navigation

--- resources/views/web/home/_kategori.blade.php
+++ resources/views/web/home/_kategori.blade.php
@@ -1,10 +1,10 @@
 @forelse ($kategori as $item)
+<a href="/kategori/{{ $item->slug }}">
 <div class="d-flex align-items-center justify-content-between gap-3">
-    <a href="/kategori/{{ $item->slug }}">
         <p>{{ $item->nama_kategori }}</p>
         <span class="badge bg-primary">{{ $item->posts_count }}</span>
-    </a>
-</div>
+    </div>
+</a>
 @empty
     
 @endforelse
